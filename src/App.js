eae9a079c6d2d52da7ecda1d1f1fcb4b2a0501fa
YH803118 <dudghks1045@naver.com>
2023-02-19 18:54:05 +0900

    scroll end

--- src/App.js
+++ src/App.js
@@ -1,65 +1,32 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-
-const normalColor = "rgba(255, 255, 255, 0.7)";
-const selectedColor = "rgba(216, 191, 216, 1)";
+const normalColor = "rgb(40,40,40)";
+const selectedColor = "rgba(200, 200, 200, 1)";
 
 function App() {
-  const [section, setSection] = useState();
+  const lastUpdate = new Intl.DateTimeFormat("kr").format(new Date());
+
+  const section = document.querySelectorAll(".section");
   const naviBtn = document.querySelectorAll("button");
-  useEffect(() => {
-    setSection(document.querySelectorAll(".section"));
-    // setNaviBtn(document.querySelectorAll("button"));
-  }, []);
-  console.log(naviBtn);
+
+  window.addEventListener("scroll", () => {
+    const scroll = window.scrollY;
+    colorReturn();
+    naviBtn[Math.floor(scroll / window.innerHeight)].style.color = selectedColor;
+  });
+
+  const colorReturn = () => {
+    for (let j = 0; j < naviBtn.length; j++) {
+      naviBtn[j].style.color = normalColor;
+    }
+  };
   for (let i = 0; i < naviBtn.length; i++) {
     naviBtn[i].addEventListener("click", () => {
       section[i].scrollIntoView({ behavior: "smooth" });
     });
   }
-  function wheelDown(section, baseElementTop, clientHeight) {
-    for (let i = 1; i < section.length; i++) {
-      if (baseElementTop === clientHeight * (section.length - i)) {
-        window.scrollTo({
-          top: clientHeight * i,
-          behavior: "smooth", // 부드럽게 이동
-        });
-        for (let j = 0; j < naviBtn.length; j++) {
-          naviBtn[j].style.color = normalColor;
-        }
-        naviBtn[i].style.color = selectedColor;
-      }
-    }
-  }
-  function wheelUp(section, baseElementTop, clientHeight) {
-    for (let i = 0; i < section.length - 1; i++) {
-      if (baseElementTop === clientHeight * i) {
-        window.scrollTo({
-          top: clientHeight * (section.length - 2 - i),
-          behavior: "smooth",
-        });
-        for (let j = 0; j < naviBtn.length; j++) {
-          naviBtn[j].style.color = normalColor;
-        }
-        naviBtn[i].style.color = selectedColor;
-      }
-    }
-  }
-  window.history.scrollRestoration = "manual";
-
-  const handleWheel = (event) => {
-    const clientHeight = window.innerHeight;
-    const baseElementTop = section[section.length - 1].getBoundingClientRect().top;
-    if (event.deltaY > 0) {
-      wheelDown(section, baseElementTop, clientHeight);
-    }
-    if (event.deltaY < 0) {
-      wheelUp(section, baseElementTop, clientHeight);
-    }
-  };
 
   return (
-    <div className="App" onScroll={handleWheel}>
+    <div className="App">
       <div className="LeftBar">
         <div className="MenuBar">
           <div className="Menu">
@@ -68,7 +35,7 @@ function App() {
             <button>섹션3</button>
             <button>섹션4</button>
           </div>
-          <p className="lastUpdate">Last Update: 2023/02/19</p>
+          <p className="lastUpdate">Last Update: {lastUpdate}</p>
         </div>
         <div className="vLine" />
       </div>
