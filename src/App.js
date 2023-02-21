675ca9338301a37239d5fb86e0948a448dcfe102
YH803118 <dudghks1045@naver.com>
2023-02-21 13:23:13 +0900

    menu setting

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ function App() {
   useEffect(() => {
     const section = document.querySelectorAll(".section");
     const naviBtn = document.querySelectorAll(".sectionBtn");
+    const subItem = document.querySelectorAll(".subItem");
     naviBtn[0].style.color = selectedColor;
     window.addEventListener("scroll", () => {
       const scroll = window.scrollY;
       colorReturn();
-      naviBtn[Math.floor(scroll / window.innerHeight)].style.color = selectedColor;
+      const page = Math.floor(scroll / window.innerHeight);
+      naviBtn[page].style.color = selectedColor;
+      subItem[page].style.display = "block";
     });
 
     const colorReturn = () => {
       for (let j = 0; j < naviBtn.length; j++) {
         naviBtn[j].style.color = normalColor;
+        subItem[j].style.display = "none";
       }
     };
     for (let i = 0; i < naviBtn.length; i++) {
@@ -39,9 +43,20 @@ function App() {
           <div className="MenuBar">
             <div className="Menu">
               <button className="sectionBtn">자기소개</button>
+              <div className="subItem"></div>
               <button className="sectionBtn">프로젝트</button>
+              <div className="subItem">
+                <button>빅데이터 기반 음악 추천</button>
+                <button>스터디 구인 사이트</button>
+                <button>쇼핑 사이트</button>
+              </div>
               <button className="sectionBtn">학력/활동</button>
+              <div className="subItem">
+                <button>청운대학교</button>
+                <button>국비교육 이수</button>
+              </div>
               <button className="sectionBtn">섹션4</button>
+              <div className="subItem"></div>
             </div>
             <p className="lastUpdate">Last Update: {lastUpdate}</p>
           </div>
@@ -50,6 +65,7 @@ function App() {
         <div className="Content">
           <div className="section">section1 - 간략한 자기소개</div>
           <div className="section">section2 - 프로젝트</div>
+
           <div className="section">section3 - 학력/활동</div>
           <div className="section">section4</div>
         </div>
