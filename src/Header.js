daa6d91eb6ff623ddfd09aea546c5906672b1207
YH803118 <dudghks1045@naver.com>
2023-02-28 20:08:35 +0900

    mobile ok

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,22 @@
 import * as React from "react";
 import "./css/Header.css";
 import profile from "./img/img.jpg";
+import mail from "./img/mail.png";
 const Header = () => {
   return (
     <>
       <div className="Header">
         <h1>_ Profile</h1>
         <div>
-          <img src={profile} />
+          <img src={profile} className="profileImg" />
           <p>
             "안녕하세요. <br />
             문제를 직시하고 타협하지 않는
             <br />
             신입 개발자 김영환입니다"
+            <br />
+            <br />
+            <img src={mail} className="icon" /> dudghks1045@naver.com
           </p>
         </div>
       </div>
